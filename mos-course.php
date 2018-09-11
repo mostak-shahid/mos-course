6c3fe28bcf70b857b9c1352b65cdd66e7541d0c7
Md. Mostak Shahid <mostak.shahid@gmail.com>
2018-09-12 01:08:02 +0600

    1.0.0

--- mos-course.php
+++ mos-course.php
@@ -3,7 +3,7 @@
 Plugin Name: Mos Course
 Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
 Description: Unlike other available WordPress learning managment system (LMS) plugins, <a href="http://mostak.belocal.today/" target="_blank" rel="nofollow">MOS Courses</a> plugin is free, has no recurring subscription fees and has has plenty of features out of the box. Create as many courses and lessons as you’d like and even require users sign up before viewing course content if you’d like. A premium extension will available soon which allows you to sell members only access to your courses.
-Version: 1.0
+Version: 1.0.0
 Author: Md. Mostak Shahid
 Author URI: http://mostak.belocal.today/
 License: GPL2
@@ -13,3 +13,9 @@ require_once ( plugin_dir_path( __FILE__ ) . 'mos-course-settings.php' );
 require_once ( plugin_dir_path( __FILE__ ) . 'mos-course-post-types.php' );
 require_once ( plugin_dir_path( __FILE__ ) . 'mos-course-taxonomy.php' );
 require_once ( plugin_dir_path( __FILE__ ) . 'mos-course-metabox.php' );
+require_once('plugins/update/plugin-update-checker.php');
+$pluginInit = Puc_v4_Factory::buildUpdateChecker(
+	'https://raw.githubusercontent.com/mostak-shahid/update/master/mos-course.json',
+	__FILE__,
+	'mos-course'
+);
